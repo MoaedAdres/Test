221a08b58c6b94f7d1f258ff3801981ccaa4c8cf
unknown <moaedadres22@gmail.com>
2023-10-21 23:23:58 +0300

    'hello'

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './assets/normlize.css'
+import './App.scss' 
+
+function App() {
+  return (
+    <>
+    <div>
+      <button>hsel</button>
+    </div>
+    </>
+  )
+}
+
+export default App
